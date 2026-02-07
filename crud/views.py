898fe3e64e1b2fd2347c99bf22187dbc31f71d3d
Matheus Henrique <teus.henrique021@gmail.com>
2026-02-06 22:02:28 -0300

    Teste de API

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+# Hello world
+def teste(request: HttpRequest):
+    return HttpResponse('Hello world!')
