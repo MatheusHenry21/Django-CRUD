19fc4a5a0cb9c32d33e8193de45911809d4c1755
Matheus Henrique <teus.henrique021@gmail.com>
2026-02-06 22:07:01 -0300

    Criação do Model

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Model de Agenda de Cll
+class Contato(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
